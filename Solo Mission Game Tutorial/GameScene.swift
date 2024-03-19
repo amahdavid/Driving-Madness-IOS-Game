46486fea6315cba0216fde1d41e5ba2b0ee56cf7
amahdavid <amahdavid34@gmail.com>
2024-03-19 12:57:13 -0700

    Solo-Mission-Tut: Made game scene with bullet background, added shooting functionality

--- Solo Mission Game Tutorial/GameScene.swift
+++ Solo Mission Game Tutorial/GameScene.swift
@@ -9,80 +9,46 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    // sets the player image
+    let player = SKSpriteNode(imageNamed: "playerShip")
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    // this function runs as soon as the scene loads up
     override func didMove(to view: SKView) {
+        // sets the background image
+        let background = SKSpriteNode(imageNamed: "background")
+        // sets the background size to the scene size
+        background.size = self.size
+        // gets the center point of the scene
+        background.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        // layering of the bakground, we want this to be at the bottom of our game
+        // the lower the number the further back the object is
+        background.zPosition = 0
+        self.addChild(background)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // how big the ship will be, 1 being normal, 2 being huge
+        player.setScale(1)
+        player.position = CGPoint(x: self.size.width / 2, y: self.size.height * 0.2)
+        player.zPosition = 2
+        self.addChild(player)
+    }
+    
+    func fireBullet() {
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.setScale(1)
+        bullet.position = player.position
+        bullet.zPosition = 1
+        self.addChild(bullet)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
+        let deleteBullet = SKAction.removeFromParent()
+        // sets the sequence of how things should happen
+        // once you shot the bullet delete it after
+        let bulletSequence = SKAction.sequence([moveBullet, deleteBullet])
+        bullet.run(bulletSequence)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+    // when we touch the screen, fire a bullet
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        fireBullet()
     }
 }
--- Solo Mission Game Tutorial/GameViewController.swift
+++ Solo Mission Game Tutorial/GameViewController.swift
@@ -13,22 +13,17 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
+            let scene = GameScene(size: CGSize(width: 1536, height: 2048))
             view.showsFPS = true
             view.showsNodeCount = true
-        }
+            view.ignoresSiblingOrder = true
+            
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            // Present the scene
+            view.presentScene(scene)
+            }
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
